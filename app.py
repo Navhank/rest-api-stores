7384cb30fe3885ce2653850432266fc8f96bacc3
Lluis Florit <lluis.florit.b@gmail.com>
2021-05-21 02:20:46 +0200

    141. A recap of the code + a few changes! Addendum

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-db_url = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("://", "ql://", 1) # "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "superSecretAndSecure"

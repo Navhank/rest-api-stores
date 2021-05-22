dc4452a94031e57e30bf3a67b10a80b48aa4b477
Lluis Florit <lluis.florit.b@gmail.com>
2021-05-22 03:27:39 +0200

    153. Logging users out in our REST API

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask.json import jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -36,7 +36,7 @@ def add_claims_to_jwt(identity):
 
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(_decripted_head, decrypted_body):
-    return decrypted_body["sub"] in BLACKLIST
+    return decrypted_body["jti"] in BLACKLIST
 
 @jwt.expired_token_loader
 def expired_token_callback(): # _decripted_head, _decrypted_body in args
@@ -81,6 +81,7 @@ api.add_resource(StoreList, "/stores")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
 api.add_resource(TokenRefresh, "/refresh")
 
 if __name__ == "__main__":

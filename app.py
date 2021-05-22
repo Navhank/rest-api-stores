fb065020fe34ce000883a3b52d493a049f196302
Lluis Florit <lluis.florit.b@gmail.com>
2021-05-22 02:55:55 +0200

    152. Blacklisting with Flask-JWT-Extended

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
 app.secret_key = "superSecretAndSecure"
 api = Api(app)
 
@@ -31,8 +34,12 @@ def add_claims_to_jwt(identity):
         return {"is_admin": True}
     return {"is_admin": False}
 
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(_decripted_head, decrypted_body):
+    return decrypted_body["sub"] in BLACKLIST
+
 @jwt.expired_token_loader
-def expired_token_callback():
+def expired_token_callback(): # _decripted_head, _decrypted_body in args
     return jsonify({
         "description": "The token has expired.",
         "error": "token_expired"
@@ -53,14 +60,14 @@ def missing_token_callback(error):
     }), 401
 
 @jwt.needs_fresh_token_loader
-def token_not_fresh_callback():
+def token_not_fresh_callback(): # _decripted_head, _decrypted_body in args
     return jsonify({
         "description": "The token is not fresh.",
         "error": "fresh_token_required"
     }), 401
 
 @jwt.revoked_token_loader
-def revoked_token_callback():
+def revoked_token_callback(_decripted_head, _decrypted_body):
     return jsonify({
         "description": "The token has been revoked.",
         "error": "token_revoked"
